e555ce9fde258ef04dbb371f74754560b25bf31e
Steve Pemberton <57773252+StevePem@users.noreply.github.com>
2024-05-07 11:06:44 +1000

    Add files via upload

---
+++ styles/slope_6_style.js
@@ -0,0 +1,72 @@
+var size = 0;
+var placement = 'point';
+function categories_slope_6(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                switch(value.toString()) {case 'Up to 1%':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(253,231,37,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.95}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '> 1% up to 2.5%':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(93,201,99,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.95}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '> 2.5% up to 5%':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(33,144,141,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.95}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '> 5% up to 10%':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(59,82,139,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.95}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '> 10%':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(68,1,84,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.95}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_slope_6 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("slope");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+var style = categories_slope_6(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
---
+++ styles/speedlimit_8_style.js
@@ -0,0 +1,80 @@
+var size = 0;
+var placement = 'point';
+function categories_speedlimit_8(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                switch(value.toString()) {case 'Up to 30 km/h':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(253,231,37,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.14}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '> 30 km/h up to 40 km/h':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(122,209,81,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.14}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '> 40 km/h up to 50 km/h':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(65,67,135,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.14}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '> 50 km/h up to 60 km/h':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(68,1,84,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.14}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '> 60 km/h hp to 80 km/h':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,38,254,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.9}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '> 80 km/h':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.9}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_speedlimit_8 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("speed_limit");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+var style = categories_speedlimit_8(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
